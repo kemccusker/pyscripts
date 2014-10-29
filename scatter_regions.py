dc1ac822b1622c8f69be421f8824f4bedc4686c6
kemccusker <kellymccusker@gmail.com>
2014-10-29 09:39:31 -0700

    Add option to have 2 variables, 2 regions, and 2 seasons in the scatter plot

--- scatter_regions.py
+++ scatter_regions.py
@@ -2,36 +2,62 @@
 import cccmautils as cutl
 import constants as con
 import cccmaplots as cplt
+import cccmacmaps as ccm
 import pandas as pd
 
-field='st'
-ncfield=field.upper()
-sea='DJF'
-region1='polcap65'
-region2='eurasia'
+printtofile=True
 
-sims = ('E1','E2','E3','E4','E5','R1','R2','R3','R4','R5')
+field1='sia'; ncfield1='SICN'
+region1='bksmori' #'polcap65'
+sea1='DJF'
+
+field2='st'; ncfield2='ST'
+region2='eurasiamori'
+sea2='DJF'
+
+sims = ('E1','E2','E3','E4','E5','R1','R2','R3','R4','R5','HAD','NSIDC','ENS','ENSE')
 
 flddregdt= {}
 flddregdt2 = {}
 
 for sim in sims:
 
-    fnamec,fnamep=con.build_filepathpair(sim,field)
+    fnamec,fnamep=con.build_filepathpair(sim,field1)
 
-    fldc=cnc.getNCvar(fnamec,ncfield,timesel='0002-01-01,0121-12-31',seas=sea)
-    fldp=cnc.getNCvar(fnamep,ncfield,timesel='0002-01-01,0121-12-31',seas=sea)
+    fldc=cnc.getNCvar(fnamec,ncfield1,timesel='0002-01-01,0121-12-31',seas=sea)
+    fldp=cnc.getNCvar(fnamep,ncfield1,timesel='0002-01-01,0121-12-31',seas=sea)
 
 
     lat=cnc.getNCvar(fnamec,'lat')
     lon=cnc.getNCvar(fnamec,'lon')
 
-    fldcreg=cutl.calc_regmean(fldc,lat,lon,region1)
-    fldpreg=cutl.calc_regmean(fldp,lat,lon,region1)
-    flddregdt[sim] = np.mean(fldpreg-fldcreg,axis=0)
-    
-    fldcreg2=cutl.calc_regmean(fldc,lat,lon,region2)
-    fldpreg2=cutl.calc_regmean(fldp,lat,lon,region2)
+    if field1=='sia':
+        fldcreg=cutl.calc_regtotseaicearea(fldc,lat,lon,region1) # isarea=False
+        fldpreg=cutl.calc_regtotseaicearea(fldp,lat,lon,region1) # isarea=False
+    else:
+        fldcreg=cutl.calc_regmean(fldc,lat,lon,region1)
+        fldpreg=cutl.calc_regmean(fldp,lat,lon,region1)
+        
+    flddregdt[sim] = np.mean(fldpreg-fldcreg,axis=0) # time mean
+
+    if field2 != field1:
+        fnamec,fnamep=con.build_filepathpair(sim,field2)
+        fldc2=cnc.getNCvar(fnamec,ncfield2,timesel='0002-01-01,0121-12-31',seas=sea2)
+        fldp2=cnc.getNCvar(fnamep,ncfield2,timesel='0002-01-01,0121-12-31',seas=sea2)
+    elif sea2 != sea1:
+        fldc2=cnc.getNCvar(fnamec,ncfield2,timesel='0002-01-01,0121-12-31',seas=sea2)
+        fldp2=cnc.getNCvar(fnamep,ncfield2,timesel='0002-01-01,0121-12-31',seas=sea2)
+    else:
+        fldc2=fldc
+        fldp2=fldp
+
+    if field2=='sia':
+        fldcreg2=cutl.calc_regtotseaicearea(fldc,lat,lon,region1) # isarea=False
+        fldpreg2=cutl.calc_regtotseaicearea(fldp,lat,lon,region1) # isarea=False
+    else:
+        fldcreg2=cutl.calc_regmean(fldc2,lat,lon,region2)
+        fldpreg2=cutl.calc_regmean(fldp2,lat,lon,region2)
+        
     flddregdt2[sim] = np.mean(fldpreg2-fldcreg2,axis=0)
 
 ser1 = pd.Series(flddregdt)
@@ -41,10 +67,29 @@ df = pd.DataFrame([ser1,ser2],index=(region1,region2))
 
 print df
 
-plt.figure()
-plt.scatter(df.values[0],df.values[1])
-plt.scatter(df.filter(regex='E').values[0],df.filter(regex='E').values[1],color='r')
+cd = ccm.get_colordict()
 
+fig,ax = plt.subplots(1) #plt.figure()
+plt.scatter(df.filter(regex='R').values[0],df.filter(regex='R').values[1],color='k',marker='s',s=8**2)
+plt.scatter(df.filter(regex='E').values[0],df.filter(regex='E').values[1],color='0.5',marker='*',s=8**2)
+plt.scatter(df.filter(regex='HAD').values[0],df.filter(regex='HAD').values[1],color=cd['HAD'],s=8**2)
+plt.scatter(df.filter(regex='NSIDC').values[0],df.filter(regex='NSIDC').values[1],color=cd['NSIDC'],s=8**2)
+plt.scatter(df.filter(regex='ENS').values[0],df.filter(regex='ENS').values[1],color=cd['ENS'],s=10**2)
+plt.scatter(df.filter(regex='ENSE').values[0],df.filter(regex='ENSE').values[1],color=cd['ENSE'],s=10**2)
+
+plt.legend(('TOT','ANT','HAD','NSIDC','$\overline{TOT}$','$\overline{ANT}$'),loc='best',fancybox=True,framealpha=0.5)
+plt.xlabel(sea1 + ' ' + field1 + ' ' + region1)
+plt.ylabel(sea2 + ' ' + field2 + ' ' + region2)
+axylims = ax.get_ylim()
+if axylims[0]<=0 and axylims[1]>=0:
+    ax.axhline(y=0,color='k',linewidth=.5)
+axxlims = ax.get_xlim()
+if axxlims[0]<=0 and axxlims[1]>=0:
+    ax.axvline(x=0,color='k',linewidth=.5)
+
+if printtofile:
+    fig.savefig('scatter_' + field1 + region1 + sea1 + '_v_' + field2 + region2 + sea2 + '.pdf')
+    
 #flddregdf = pd.DataFrame(flddregdt,i)
 
 ## plt.figure()
