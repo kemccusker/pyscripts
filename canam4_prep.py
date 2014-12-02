1e39b4454b88d87ecc929ee46f027a60dcbd9f5a
kemccusker <kellymccusker@gmail.com>
2014-12-01 18:39:07 -0800

    Make sicn plots. make seasonal cycle plot. Make vert plot

--- canam4_prep.py
+++ canam4_prep.py
@@ -27,20 +27,20 @@ plt.ion()
 
 printtofile=True
 
-field = 'pmsl'
-smclim=False
+field = 't'
+smclim=True
 level=50000 # for threed
 
 addcont=False # overlay map with contours
 sigoff=False # if True, don't add significance
-field2='gz' 
+field2='pmsl' 
 level2=50000
 
 # seasonalmap, seasonalvert, plotzonmean, plotseacyc, pattcorrwithtime, plotregmean, timetosig, timetosigsuper
-plottype='seasonalmap' 
+plottype='seasonalvert' 
 
 # None, polcap60, polcap65, polcap70, eurasia, eurasiamori, ntham, nthatl, bks, bksmori, soo
-region='ntham'
+region=None#'ntham'
 screen=True
 seacyclatlim=60
 withlat=False
@@ -64,7 +64,7 @@ canens=False # just the CAN ensemble (E1-E5) plus mean, plus mean of R ensemble.
 allens=False # this is ONLY the ensemble means, plus superensemble
 sensruns=False # sensruns only: addr4ct=1,addsens=1. others=0 no meanBC, r mean, or obs
 ivar=False # this will show ENS (TOT) and ENSE (ANTH) and their difference = internal var
-simsforpaper=False # meanBC, HAD, NSIDC only. best for maps and zonal mean figs (not line plots)
+simsforpaper=True # meanBC, HAD, NSIDC only. best for maps and zonal mean figs (not line plots)
 antcat=False # this is the concatenation of ens members within each ensemble (really only useful for ANT)
 bothcat=False # can do concatenation of both ensembles if want to. These are useful for timetosig
 
