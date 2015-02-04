1d53f8576ab870f6039fdca4b669091e3b14acb8
kemccusker <kellymccusker@gmail.com>
2015-02-03 18:47:24 -0800

    Attempt to use pandas to create monthly climos. Not working yet

--- volcanoesLE.py
+++ volcanoesLE.py
@@ -1,23 +1,35 @@
 
 import pandas as pd
 import cccmaplots as cplt
+import datetime as datetime
+from netCDF4 import num2date
 
 # Agung: Feb. 1963
 # Chichon: Apr 1982
 # Pinatubo: Jul 1991
 
 # These time selections start with month of eruption
-timselagung ='1963-02-01,1965-12-31'
-timselchichon = '1982-04-01,1985-12-31'
-timselpinatubo = '1991-07-01,1993-12-31'
+## timselagung ='1963-02-01,1965-12-31'
+## timselchichon = '1982-04-01,1985-12-31'
+## timselpinatubo = '1991-07-01,1993-12-31'
 
-timselbase = '1960-01-01,1999-12-31'
-basedt = le.load_LEdata(fdict,'historicalNat',timesel=timselbase,seas=('ANN',))# annual avg fine for now
-basedf=pd.DataFrame(basedt)
-tmp = basedf.loc['ANN']
-bamean = np.mean(tmp.mean(axis=0), axis=0) # ens mean and time mean
+# These time selections start with beginning of year of eruption
+#  (so can easily remove seasonal cycle)
+timselagung ='1963-01-01,1965-12-31'
+timselchichon = '1982-01-01,1985-12-31'
+timselpinatubo = '1991-01-01,1993-12-31'
 
 
+## timselbase = '1960-01-01,1999-12-31'
+## basedt = le.load_LEdata(fdict,'historicalNat',timesel=timselbase,seas=('ANN',))# annual avg fine for now
+## basedf=pd.DataFrame(basedt)
+## tmp = basedf.loc['ANN']
+## bamean = np.mean(tmp.mean(axis=0), axis=0) # ens mean and time mean
+
+
+# Need to remove each run's annual cycle
+natclimo = le.load_LEdata(fdict,'historicalNat',ftype='1950-2020_climo')
+histclimo = le.load_LEdata(fdict,'historical',ftype='1950-2020_climo')
 
 agungdt=le.load_LEdata(fdict,'historicalNat',timesel=timselagung)
 chichondt=le.load_LEdata(fdict,'historicalNat',timesel=timselchichon)
@@ -25,9 +37,23 @@ pinatubodt=le.load_LEdata(fdict,'historicalNat',timesel=timselpinatubo)
 
 fname = agungdt.keys()[0]
 
-agungtime = cnc.getNCvar(fname,'time',timesel=timselagung) # not really using this yet.
 lev = cnc.getNCvar(fname,'plev')
 lat = cnc.getNCvar(fname,'lat')
+atime = cnc.getNCvar(fname,'time',timesel=timselagung)
+adates = cnc.get_NCdates(fname,timesel=timselagung)
+
+
+#atime = pd.date_range(start='1963/1/1',end='1965/12/31',freq='M')
+
+
+ctime = cnc.getNCvar(fname,'time',timselchichon)
+ptime = cnc.getNCvar(fname,'time',timselpinatubo)
+
+
+
+adt=le.load_LEdata(fdict,'historicalNat',timesel=timselagung)
+apan=pd.Panel(adt,major_axis=adates)
+amon = apan.groupby(apan.major_axis.month) # this returns a pandas.core.groupby.PanelGroupBy object. @@ what to do w/ it??
 
 # @@ Note that these are currently not weighted by month:
 agungsr=pd.Series(agungdt)
