b866207d9d1749806072c2c1afce8939edd9f7ea
kemccusker <kellymccusker@gmail.com>
2015-02-06 18:26:17 -0800

    Add volcano averaging function. Add climo contours to figs

--- volcanoesLE.py
+++ volcanoesLE.py
@@ -8,12 +8,17 @@ import loadLE as le
 
 le=reload(le)
 
-field='taZM'; ncfield='ta'; cmin=-2; cmax=2; cminz=-0.5; cmaxz=0.5
-#field='uaZM'; ncfield='ua'; cmin=-2; cmax=2; cminz=-0.5; cmaxz=0.5
+plt.close('all')
+
+
+#field='taZM'; ncfield='ta'; cmin=-2; cmax=2; cminz=-0.5; cmaxz=0.5
+field='uaZM'; ncfield='ua'; cmin=-2; cmax=2; cminz=-0.5; cmaxz=0.5
 
 printtofile=True
 
 fdict = {'field': field, 'ncfield': ncfield, 'comp': 'Amon'}
+offset=5
+duration=9
 
 # Agung: Feb. 1963
 # Chichon: Apr 1982
@@ -31,17 +36,11 @@ timselchichon = '1982-01-01,1984-12-31'; chstidx=3
 timselpinatubo = '1991-01-01,1993-12-31'; pistidx=6
 
 
-## timselbase = '1960-01-01,1999-12-31'
-## basedt = le.load_LEdata(fdict,'historicalNat',timesel=timselbase,seas=('ANN',))# annual avg fine for now
-## basedf=pd.DataFrame(basedt)
-## tmp = basedf.loc['ANN']
-## bamean = np.mean(tmp.mean(axis=0), axis=0) # ens mean and time mean
-
-
-# Need to remove each run's annual cycle
+# Need to remove each run's annual cycle: get seasonal cycle climo
 natclimo = le.load_LEdata(fdict,'historicalNat',ftype='1950-2020_climo',rettype='ndarray')
-#histclimo = le.load_LEdata(fdict,'historical',ftype='1950-2020_climo',rettype='ndarray')
+histclimo = le.load_LEdata(fdict,'historical',ftype='1950-2020_climo',rettype='ndarray')
 
+# Get months surrounding each volcano
 agungdat=le.load_LEdata(fdict,'historicalNat',timesel=timselagung, rettype='ndarray')
 chichondat=le.load_LEdata(fdict,'historicalNat',timesel=timselchichon, rettype='ndarray')
 pinatubodat=le.load_LEdata(fdict,'historicalNat',timesel=timselpinatubo, rettype='ndarray')
@@ -60,9 +59,6 @@ atime = cnc.getNCvar(fname,'time',timesel=timselagung)
 adates = cnc.get_NCdates(fname,timesel=timselagung)
 
 
-#atime = pd.date_range(start='1963/1/1',end='1965/12/31',freq='M')
-
-
 ctime = cnc.getNCvar(fname,'time',timselchichon)
 ptime = cnc.getNCvar(fname,'time',timselpinatubo)
 
@@ -73,102 +69,169 @@ nyr=ntime/12.
 #   for wind only
 
 #======= Agung
-anatrem = np.zeros(agungdat.shape)
+## anatrem = np.zeros(agungdat.shape)
 
-for nii in range(0,numens):
+## for nii in range(0,numens):
     
-    # tile the climo
-    climo = np.squeeze(natclimo[nii,...])
-    ag = np.squeeze(agungdat[nii,...])
+##     # tile the climo
+##     climo = np.squeeze(natclimo[nii,...])
+##     ag = np.squeeze(agungdat[nii,...])
     
-    nspace = climo.shape[1]
-    climot=np.tile(climo,(nyr,1))
+##     nspace = climo.shape[1]
+##     climot=np.tile(climo,(nyr,1))
 
-    anatrem[nii,...] = ag-climot
+##     anatrem[nii,...] = ag-climot
 
-anatrem = anatrem.reshape((numens,ntime,nlev,nlat))
+## anatrem = anatrem.reshape((numens,ntime,nlev,nlat))
 
-# Now select the 9 months starting 5 months after eruption
-agslice=slice(agstidx+5,agstidx+14)
+## # Now select the 9 months starting 5 months after eruption
+## agslice=slice(agstidx+5,agstidx+14)
 
-acomptime=anatrem.mean(axis=0)
-acomp = acomptime[agslice,...].mean(axis=0)
+## acomptime=anatrem.mean(axis=0)
+## acomp = acomptime[agslice,...].mean(axis=0)
 
-#======= Chichon
-cnatrem = np.zeros(chichondat.shape)
 
-for nii in range(0,numens):
+def volcano_avg(volcdat,remclimo, eridx, coords,offset=5, duration=9):
+    """ volcano_comp(volcdat,remclimo):
+            For each ensemble member, remove its climo sea cycle,
+            then select months to average.
+            
+            ermon: eruption index into timeseries
+            Default offset months is 5 months after eruption
+            Default duration is 9 months
+
+            returns the average of volcano time period over ensemble
+    """
+    volcrem = np.zeros(volcdat.shape)
+    (numens,ntime,nlevlat) = volcdat.shape
+    n1=coords[0]
+    n2=coords[1]
     
-    # tile the climo
-    climo = np.squeeze(natclimo[nii,...])
-    ch = np.squeeze(chichondat[nii,...])
+
+    for nii in range(0,numens):
+
+        # tile the climo
+        climo = np.squeeze(remclimo[nii,...])
+        vo = np.squeeze(volcdat[nii,...])
+
+        nspace = climo.shape[1]
+        climot=np.tile(climo,(nyr,1))
+
+        volcrem[nii,...] = vo-climot
+
+    volcrem = volcrem.reshape((numens,ntime,n1,n2))
+
+    # Now select the 9 months starting 5 months after eruption
+    voslice=slice(eridx+offset,eridx+offset+duration)
+
+    vcomptime=volcrem.mean(axis=0)
+    vcomp = vcomptime[voslice,...].mean(axis=0)
+
+    return vcomp
+
+
+acomp = volcano_avg(agungdat,natclimo, agstidx,(nlev,nlat))
+aclim = natclimo.mean(axis=0)
+aclim = aclim[agstidx+offset:agstidx+offset+duration,...].mean(axis=0)
+aclim = np.reshape(aclim,(nlev,nlat))
+
+ccomp = volcano_avg(chichondat,natclimo, chstidx,(nlev,nlat))
+cclim = natclimo.mean(axis=0)
+cclim = cclim[chstidx+offset:chstidx+offset+duration,...].mean(axis=0)
+cclim = np.reshape(aclim,(nlev,nlat))
+
+pcomp = volcano_avg(pinatubodat,natclimo, pistidx,(nlev,nlat))
+pclim = natclimo.mean(axis=0)
+pclim = pclim[pistidx+offset:pistidx+offset+duration,...].mean(axis=0)
+pclim = np.reshape(aclim,(nlev,nlat))
+
+
+## #======= Chichon
+## cnatrem = np.zeros(chichondat.shape)
+
+## for nii in range(0,numens):
     
-    nspace = climo.shape[1]
-    climot=np.tile(climo,(nyr,1))
-
-    cnatrem[nii,...] = ch-climot
-
-cnatrem = cnatrem.reshape((numens,ntime,nlev,nlat))
-
-# Now select the 9 months starting 5 months after eruption
-chslice=slice(chstidx+5,chstidx+14)
-
-ccomptime=cnatrem.mean(axis=0)
-ccomp = ccomptime[chslice,...].mean(axis=0)
-
-
-
-#======= Chichon
-pnatrem = np.zeros(pinatubodat.shape)
-
-for nii in range(0,numens):
+##     # tile the climo
+##     climo = np.squeeze(natclimo[nii,...])
+##     ch = np.squeeze(chichondat[nii,...])
     
-    # tile the climo
-    climo = np.squeeze(natclimo[nii,...])
-    pi = np.squeeze(pinatubodat[nii,...])
+##     nspace = climo.shape[1]
+##     climot=np.tile(climo,(nyr,1))
+
+##     cnatrem[nii,...] = ch-climot
+
+## cnatrem = cnatrem.reshape((numens,ntime,nlev,nlat))
+
+## # Now select the 9 months starting 5 months after eruption
+## chslice=slice(chstidx+5,chstidx+14)
+
+## ccomptime=cnatrem.mean(axis=0)
+## ccomp = ccomptime[chslice,...].mean(axis=0)
+
+
+
+## #======= Chichon
+## pnatrem = np.zeros(pinatubodat.shape)
+
+## for nii in range(0,numens):
     
-    nspace = climo.shape[1]
-    climot=np.tile(climo,(nyr,1))
+##     # tile the climo
+##     climo = np.squeeze(natclimo[nii,...])
+##     pi = np.squeeze(pinatubodat[nii,...])
+    
+##     nspace = climo.shape[1]
+##     climot=np.tile(climo,(nyr,1))
 
-    pnatrem[nii,...] = pi-climot
+##     pnatrem[nii,...] = pi-climot
 
-pnatrem = pnatrem.reshape((numens,ntime,nlev,nlat))
+## pnatrem = pnatrem.reshape((numens,ntime,nlev,nlat))
 
-# Now select the 9 months starting 5 months after eruption
-pislice=slice(pistidx+5,pistidx+14)
+## # Now select the 9 months starting 5 months after eruption
+## pislice=slice(pistidx+5,pistidx+14)
 
-pcomptime=pnatrem.mean(axis=0)
-pcomp = pcomptime[pislice,...].mean(axis=0)
+## pcomptime=pnatrem.mean(axis=0)
+## pcomp = pcomptime[pislice,...].mean(axis=0)
 
 
 # #######plotting #####################
 
 levlim=10
 
+levs,lats=np.meshgrid(lat,lev/100.)
 
 fig,axs=plt.subplots(1,3)
 fig.set_size_inches(13,6)
 
 ax=axs[0]
-cplt.vert_plot(acomp,lev,lat,axis=ax,cmin=cmin,cmax=cmax,title='agung',suppcb=True,levlim=levlim,addcontlines=True)
+cplt.vert_plot(acomp,lev,lat,axis=ax,cmin=cmin,cmax=cmax,title='agung',suppcb=True,
+               levlim=levlim,addcontlines=True,cmap='blue2red_20')
+ax.contour(levs,lats,aclim,colors='0.5',linewidths=2)
+
 ax=axs[1]
-cplt.vert_plot(ccomp,lev,lat,axis=ax,cmin=cmin,cmax=cmax,title='chichon',suppcb=True,suppylab=True,levlim=levlim,addcontlines=True)
+cplt.vert_plot(ccomp,lev,lat,axis=ax,cmin=cmin,cmax=cmax,title='chichon',suppcb=True,
+               suppylab=True,levlim=levlim,addcontlines=True,cmap='blue2red_20')
+ax.contour(levs,lats,cclim,colors='0.5',linewidths=2)
+
 ax=axs[2]
-vp = cplt.vert_plot(pcomp,lev,lat,axis=ax,cmin=cmin,cmax=cmax,title='pinatubo',suppcb=True,suppylab=True,levlim=levlim,addcontlines=True)
+vp = cplt.vert_plot(pcomp,lev,lat,axis=ax,cmin=cmin,cmax=cmax,title='pinatubo',suppcb=True,
+                    suppylab=True,levlim=levlim,addcontlines=True,cmap='blue2red_20')
+ax.contour(levs,lats,pclim,colors='0.5',linewidths=2)
 
 cbar_ax = fig.add_axes([.25,0.07, 0.5, .02])
 cbor='horizontal'
 fig.colorbar(vp,cax=cbar_ax, orientation=cbor) 
 if printtofile:
-    fig.savefig('agung_chichon_pinatubo_' + field + '.pdf')
+    fig.savefig('agung_chichon_pinatubo_' + field + '_climocont.pdf')
 
 
 fig,ax=plt.subplots(1,1)
 #fig.set_size_inches(12,6)
 cplt.vert_plot((acomp+ccomp+pcomp)/3.,lev,lat,axis=ax,cmin=cmin,cmax=cmax,
-               title='volc composite',levlim=levlim,addcontlines=True)
+               title='volc composite',levlim=levlim,addcontlines=True,cmap='blue2red_20')
+ax.contour(levs,lats,(aclim+cclim+pclim)/3.,colors='0.5',linewidths=2)
+
 if printtofile:
-    fig.savefig('volc_composite_' + field + '.pdf')
+    fig.savefig('volc_composite_' + field + '_climocont.pdf')
 
 
 # zoom in
@@ -176,10 +239,12 @@ if printtofile:
 fig,ax=plt.subplots(1,1)
 #fig.set_size_inches(12,6)
 cplt.vert_plot((acomp+ccomp+pcomp)/3.,lev,lat,axis=ax,cmin=cminz,cmax=cmaxz,
-               title='volc composite',levlim=100,addcontlines=True)
+               title='volc composite',levlim=100,addcontlines=True,cmap='blue2red_20')
+ax.contour(levs,lats,(aclim+cclim+pclim)/3.,colors='0.5',linewidths=2)
+
 ax.set_xlim(-30,30)
 if printtofile:
-    fig.savefig('volc_composite_' + field + '_zoom.pdf')
+    fig.savefig('volc_composite_' + field + '_zoom_climocont.pdf')
 
 """
 #adt=le.load_LEdata(fdict,'historicalNat',timesel=timselagung)
