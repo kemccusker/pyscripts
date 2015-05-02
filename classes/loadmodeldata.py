edd0788c54d8973715ae00ad13af3e3aee23f8ad
kemccusker <kellymccusker@gmail.com>
2015-05-01 18:11:16 -0700

    update loaddata to accept region, filetype. remove loop through times

--- classes/loadmodeldata.py
+++ classes/loadmodeldata.py
@@ -5,6 +5,7 @@
 import numpy as np
 import constants as con
 import cccmaNC as cnc
+import cccmautils as cutl
 
 def loadmondata(fields,simulations,model='CanAM4',levsel=None,meantype=None):
     """ loadmondata(fields, simulations, model='CanAM4',levsel=None, meantype=None)
@@ -49,11 +50,13 @@ def loadseasdata(fields,simulations,model='CanAM4',levsel=None,meantype=None):
     return loaddata(fields,simulations,model,timefreq='seasonal',levsel=levsel,meantype=meantype)
 
 
-def loaddata(fields, simulations, model='CanAM4',timeper='001-121',timefreq=None, levsel=None,meantype=None):
-    """ loaddata(fields, simulations,model='CanAM4',timeper='001-121',timefreq=None, levsel=None, meantype=None)
+def loaddata(fields, simulations, model='CanAM4',timeper='001-121',timefreq=None, 
+             levsel=None,meantype=None,filetype='diff',region=None):
+    """ loaddata(fields, simulations,model='CanAM4',timeper='001-121',timefreq=None, 
+                 levsel=None, meantype=None,filetype='diff',region=None)
     
             fields: tuple of field names
-            simulations: tuple of simulation names
+            simulations: tuple of simulation names (diff names='E1'...'ENS','NSIDC' etc)
             model: for now only 'CanAM4' is implemented
             timeper: time period of the data (used for filename). default '001-121'
             timefreq: time frequency TO RETURN. default all data
@@ -64,6 +67,9 @@ def loaddata(fields, simulations, model='CanAM4',timeper='001-121',timefreq=None
                        one if loading multiple variables and multiple simulations at once.
                        It is assumed that time is the first dimension.
                        If 'time' is the meantype, datadict contains tuples of (mean,std)
+            filetype: 'diff','ctl','pert'
+                       Default is diff where both ctl and pert are read in and differenced.
+            region:   any of the regions in constants -> region dict. default None.
 
             returns: nested dictionary@@
                     FIELDS->SIMULATIONS->TIMEFREQ
@@ -79,8 +85,8 @@ def loaddata(fields, simulations, model='CanAM4',timeper='001-121',timefreq=None
         return -1
 
     print '@@ probably should invert the order such that it is field, season, sim?'
-    bp=con.get_basepath()
-    basepath=bp['basepath'] + model + '/'; subdir=bp['subdir']
+    #bp=con.get_basepath()
+    #basepath=bp['basepath'] + model + '/'; subdir=bp['subdir']
     timesel='0002-01-01,0121-12-31'
 
     seabool=False # set to True if requested time freq is one season or climo
@@ -106,6 +112,8 @@ def loaddata(fields, simulations, model='CanAM4',timeper='001-121',timefreq=None
     for field in fields:
 
         print field # @@
+        # assume simulations entered are of the form: E1, R3, ENSE etc. Then
+        # filetype input arg tells which simulation to get (or both) @@@@ NOT DONE.
         simdict = dict.fromkeys(simulations,{})
 
         for sim in simulations:
@@ -113,31 +121,50 @@ def loaddata(fields, simulations, model='CanAM4',timeper='001-121',timefreq=None
             timdict = dict.fromkeys(tf)
 
             # construct filename here @@
-            fname = basepath+sim+subdir+sim+'_'+field+'_'+timeper+'_ts.nc'
-            print fname
+            fnamec,fnamep = con.build_filepathpair(sim,field)
+            #fname = basepath+sim+subdir+sim+'_'+field+'_'+timeper+'_ts.nc'
+            print fnamec 
             
-            for tfkey in tf:
-                print tfkey # @@
-                # @@ get the data with cnc.getNCvar() here
-                ncparams = {}
-                if monbool:
-                    ncparams = {'monsel': tfkey}
-                elif seabool:
-                    ncparams = {'seas': tfkey}
+            tfkey = tf
+            #for tfkey in tf:
+            #    print tfkey # @@
+            #print 'too many levels in the dict...get rid of seasonal keys and just do one@@@ 5/1/2015'
+            # @@ get the data with cnc.getNCvar() here
+            ncparams = {}
+            if monbool:
+                ncparams = {'monsel': tfkey}
+            elif seabool:
+                ncparams = {'seas': tfkey}
 
-                if levsel!=None:
-                    ncparams['levsel'] = levsel
+            if levsel!=None:
+                ncparams['levsel'] = levsel
 
-                if meantype=='zonal':
-                    ncparams['calc'] = 'zm'
-                    
-                fld = cnc.getNCvar(fname,field.upper(),timesel=timesel,**ncparams)
-                if meantype=='time':
-                    fldstd = np.std(fld,axis=0)
-                    fld = np.mean(fld,axis=0)
-                    timdict[tfkey] = fld,fldstd
-                else:
-                    timdict[tfkey] = fld
+            if meantype=='zonal':
+                ncparams['calc'] = 'zm'
+
+            if filetype=='diff':
+                fld = cnc.getNCvar(fnamep,field.upper(),timesel=timesel,**ncparams) -\
+                      cnc.getNCvar(fnamec,field.upper(),timesel=timesel,**ncparams)
+            elif filetype=='ctl':
+                fld = cnc.getNCvar(fnamec,field.upper(),timesel=timesel,**ncparams)
+            elif filetype=='pert':
+                fld = cnc.getNCvar(fnamep,field.upper(),timesel=timesel,**ncparams)
+            else:
+                print "filetype not supported! ['diff'|'ctl'|'pert']"
+                return -1
+
+            if region != None:
+                lat=cnc.getNCvar(fnamec,'lat'); lon=cnc.getNCvar(fnamec,'lon')
+                fld = cutl.calc_regmean(fld,lat,lon,region)
+
+            if meantype=='time':
+                #fldstd = np.std(fld,axis=0)
+                fld = np.mean(fld,axis=0)
+                #timdict[tfkey] = fld #,fldstd
+                timdict=fld
+            else:
+                #timdict[tfkey] = fld
+                timdict=fld
 
             simdict[sim] = timdict
 
