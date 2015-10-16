63eab071844e5c94d7547abca847d914f140fece
kemccusker <kellymccusker@gmail.com>
2015-10-15 19:32:56 -0700

    fix seasonal cycle of SIE, SIV. add flipped option for maps subplot

--- plot_boundaryconditions.py
+++ plot_boundaryconditions.py
@@ -1,11 +1,11 @@
 import cccmautils as cutl
 import constants as con
 import loadmodeldata as lmd
-
+import matplotlib.lines as mlines
 
 plt.close('all')
 masksicn=True # use present day sicn to mask SST
-printtofile=False
+printtofile=True
 
 deni = 913. # kg/m3 density of ice
 timesel='0011-01-01,0011-12-31' # don't have to load much b/c constant
@@ -20,10 +20,10 @@ def load_BCs(fields, season=None, timesel='0011-01-01,0011-12-31'):
     simpbase='kem1pert2'
     obsbase = 'kemnsidc'
 
-    flddat={}; sicnpdat={}
+    flddat={}; fldcdat={}; fldpdat={}; sicnpdat={}
     for fii,field in enumerate(fields):
         fnamesc={}; fnamesp={}
-        flddiff={}
+        flddiff={}; fldcavg={}; fldpavg={}
 
         print field
         # Loop through sim data:
@@ -56,15 +56,21 @@ def load_BCs(fields, season=None, timesel='0011-01-01,0011-12-31'):
 
             if season==None:
                 flddiff[ii] = fldp-fldc
+                fldcavg[ii] = fldc
+                fldpavg[ii] = fldp
             else:
                 flddiff[ii] = cutl.seasonalize_monthlyts(fldp-fldc,season=sea,climo=1) 
+                fldcavg[ii] = cutl.seasonalize_monthlyts(fldc,season=sea,climo=1) 
+                fldpavg[ii] = cutl.seasonalize_monthlyts(fldp,season=sea,climo=1) 
+
             fnamesc[ii]=fnamec
             fnamesp[ii]=fnamep
 
         flddat[field] = flddiff
+        fldcdat[field] = fldcavg
+        fldpdat[field] = fldpavg
 
-
-    return flddat, sicnpdat
+    return flddat, fldcdat, fldpdat, sicnpdat
 
 
 
@@ -76,25 +82,35 @@ sicn['cmin'] = -15; sicn['cmax'] = 15; sicn['cmap']='red2blue_w20'
 sic['cmin'] = -0.5; sic['cmax'] = 0.5; sic['cmap']='red2blue_w20'
 
 meta = {'gt': gt, 'sicn': sicn, 'sic': sic}
-tlabs = {'gt': '$\Delta$ SST ($^\circ$C)', 'sicn': '$\Delta$ SIC (%)', 'sic': '$\Delta$ SIT (m)'}
 ylabs = {5: 'OBS', 0: 'E1', 1: 'E2', 2: 'E3', 3: 'E4', 4:'E5'} 
 convdt = {'gt': 1, 'sicn': 100, 'sic': 1/np.float(deni)}
 
 
 # === load one year for seasonal cycle:
-flddat, sicnpdat = load_BCs(fields, season=None, timesel=timesel)
+flddat,fldcdat,fldpdat,sicnpdat = load_BCs(fields, season=None, timesel=timesel)
 
 
 # ============== PLOT ==============
 lat = con.get_t63lat() #cnc.getNCvar(fnamec,'lat')
 lon = con.get_t63lon() #cnc.getNCvar(fnamep,'lon')
 
+fliptohoriz=True
 
 fields=('sicn','sic','gt')
+tlabs = {'gt': '$\Delta$ SST ($^\circ$C)', 'sicn': '$\Delta$ SIC (%)', 'sic': '$\Delta$ SIT (m)'}
 
-fig,axs = plt.subplots(6,3)
-fig.set_size_inches((7,14))
-fig.subplots_adjust(wspace=0.1,hspace=0.05)
+if fliptohoriz:
+    psuff='_horizor' # horizontal orientation
+    # rows are fields and cols are sims
+    fig,axs = plt.subplots(3,6)
+    fig.set_size_inches((10,5))
+    fig.subplots_adjust(wspace=0.05,hspace=0.1)
+else:
+    psuff='_vertor' # vertical orientation
+    # cols are fields and rows are sims
+    fig,axs = plt.subplots(6,3)
+    fig.set_size_inches((7,14))
+    fig.subplots_adjust(wspace=0.1,hspace=0.05)
 
 for fii,field in enumerate(fields):
 
@@ -106,7 +122,10 @@ for fii,field in enumerate(fields):
 
     for ii in range(0,6):
 
-        ax=axs[ii,fii]
+        if fliptohoriz:
+            ax=axs[fii,ii]
+        else:
+            ax=axs[ii,fii]
 
         plotfld=flddiff[ii]*conv
         plotfld = cutl.seasonalize_monthlyts(plotfld,season=sea, climo=1)
@@ -120,20 +139,29 @@ for fii,field in enumerate(fields):
                             latlim=45, axis=ax, suppcb=True, 
                             round=False,lmask=True,**metap)
 
-        if ax.is_first_row(): ax.set_title(tlabs[field])
-        if ax.is_first_col(): ax.set_ylabel(ylabs[ii])
+        if fliptohoriz:
+            if ax.is_first_col(): ax.set_ylabel(tlabs[field])
+            if ax.is_first_row(): ax.set_title(ylabs[ii])
+        else:
+            if ax.is_first_row(): ax.set_title(tlabs[field])
+            if ax.is_first_col(): ax.set_ylabel(ylabs[ii])
 
-    axposx = ax.get_position().get_points()[0][0] # x position of last row
-    axwi = ax.get_position().width # width of last row
-    
-    cpos=[axposx,.07, axwi,.02]
-    cbar_ax,cbar = cplt.add_colorbar(fig,pc,orientation='horizontal',pos=cpos)
+    axposx = ax.get_position().get_points()[0][0] # x position of last ax
+    axposy = ax.get_position().get_points()[0][1] # y position of last ax
+    axwi = ax.get_position().width # width of last ax
+    axht = ax.get_position().height # height of last ax
+    if fliptohoriz:
+        cpos=[0.91, axposy,.02,axht]
+        cbar_ax,cbar = cplt.add_colorbar(fig,pc,orientation='vertical',pos=cpos)
+    else:
+        cpos=[axposx,.07, axwi,.02]
+        cbar_ax,cbar = cplt.add_colorbar(fig,pc,orientation='horizontal',pos=cpos)
     cbar.set_ticks([metap['cmin'],0,metap['cmax']])
     cbar.set_ticklabels((metap['cmin'],0,metap['cmax']))
 
 if printtofile:
-    fig.savefig('suppfig_boundaryconditions.pdf')
-    fig.savefig('suppfig_boundaryconditions.eps',format='eps',dpi=600)
+    fig.savefig('suppfig_boundaryconditions' + psuff+'.pdf',bbox_inches='tight')
+    fig.savefig('suppfig_boundaryconditions' + psuff+'.eps',bbox_inches='tight',format='eps',dpi=600)
 
 
 #tst=flddat['sicn'][1]
@@ -142,18 +170,28 @@ if printtofile:
 
 # ======== plot seasonal cycle
 
+simslg=mlines.Line2D([],[],color='0.5',linewidth=2) 
+osimlg=mlines.Line2D([],[],color='r',linewidth=2) 
+
+
+sie=False  # else it's sea ice area
 fields=('sicn','sic')
 xx=range(0,12)
 xxticks=np.arange(0,12)
 xxlabs=('Jan','','Mar','','May','','Jul','','Sep','','Nov','')
-tlabs = {'gt': '$\Delta$ SST ($^\circ$C)', 
-         'sicn': '$\Delta$ SIA (millions of km$^2$)', 
-         'sic': '$\Delta$ SIT (millions of m$^3$)'}
+tlabs = {'gt': 'SST ($^\circ$C)', 
+         'sicn': 'SIE (millions of km$^2$)', 
+         'sic': 'SIV (thousands of km$^3$)'}
 
 
 fig,axs = plt.subplots(2,1)
-fig.set_size_inches((7,7))
+fig.set_size_inches((5.2,5))
 fig.subplots_adjust(wspace=0.1,hspace=0.1)
+
+# climos!
+fig2,axs2 = plt.subplots(2,1)
+fig2.set_size_inches((5.2,5))
+fig2.subplots_adjust(wspace=0.1,hspace=0.1)
 
 for fii,field in enumerate(fields):
 
@@ -162,22 +200,40 @@ for fii,field in enumerate(fields):
     conv = convdt[field]
 
     flddiff=flddat[field]
+    fldcavg=fldcdat[field]
+    fldpavg=fldpdat[field]
 
     ax=axs[fii]
+    ax2=axs2[fii]
 
     for ii in range(0,6):
 
         simfld=flddiff[ii]
+        simcfld=fldcavg[ii]
+        simpfld=fldpavg[ii]
 
         if field=='sicn':
-            # Can't do extent here b/c only saved the difference of SICN.
-            #  doesn't make sense to calc SIE @@@@
-            #plotfld,_ = cutl.calc_seaiceextent(simfld,lat,lon,model=None)
-            plotfld,_ = cutl.calc_totseaicearea(simfld,lat,lon,model=None)
+
+            if sie:
+                pstr='SIEnh'
+                plotfldc,_ = cutl.calc_seaiceextent(simcfld,lat,lon,model=None)
+                plotfldp,_ = cutl.calc_seaiceextent(simpfld,lat,lon,model=None)
+            else: # sia
+                pstr='SIAnh'
+                tlabs['sicn'] = 'SIA (millions of km$^2$)'
+                plotfldc,_ = cutl.calc_totseaicearea(simcfld,lat,lon,model=None)
+                plotfldp,_ = cutl.calc_totseaicearea(simpfld,lat,lon,model=None)
+
+            plotfld = plotfldp-plotfldc
+            #plotfld,_ = cutl.calc_totseaicearea(simfld,lat,lon,model=None)
         elif field=='sic':
-            tmp=flddat['sicn'][ii]
-            # have to do CTL and PERT separately to get this right. @@@@@
-            plotfld,_ = cutl.calc_totseaicevol(simfld*conv,tmp,lat,lon)
+            tmpc=fldcdat['sicn'][ii]
+            tmpp=fldpdat['sicn'][ii]
+            plotfldc,_ = cutl.calc_totseaicevol(simcfld*conv,tmpc,lat,lon)
+            plotfldp,_ = cutl.calc_totseaicevol(simpfld*conv,tmpp,lat,lon)
+            plotfld = plotfldp-plotfldc
+            #print conv
+            #print 'plotfldc,plotfldp' + str(plotfldc)+',' + str(plotfldp)
         else:
             print '@@ field ' + field + ' not recognized' 
             break
@@ -187,26 +243,46 @@ for fii,field in enumerate(fields):
         else:
             clr='0.5'
         ax.plot(xx,plotfld, color=clr, linewidth=2)
+
+        ax2.plot(xx,plotfldc,color=clr,linewidth=2)
+        ax2.plot(xx,plotfldp,color=clr,linewidth=2,linestyle='--')
         
     yticks=ax.get_yticks()
-    #ax.set_yticklabels(yticks/1e12)
-    #ax.set_xticks(xxticks)
+    ax.set_yticklabels(yticks/1e12)
+    ax.set_xticks(xxticks)
     if ax.is_last_row():
         ax.set_xticklabels(xxlabs)
     else:
+        ax.legend((simslg,osimlg),('Model BCs','Observational BC'),loc='lower left',
+                  frameon=False)
         ax.set_xticklabels('')
     ax.set_xlim(0,11)
-    ax.set_ylabel(tlabs[field])
+    ax.set_ylabel('$\Delta$ ' + tlabs[field])
 
+    yticks=ax2.get_yticks()
+    ax2.set_yticklabels(yticks/1e12)
+    ax2.set_xticks(xxticks)
+    if ax2.is_last_row():
+        ax2.set_xticklabels(xxlabs)
+    else:
+        ax2.set_xticklabels('')
+    ax2.set_xlim(0,11)
+    ax2.set_ylabel(tlabs[field])
 
-tst=flddat['sicn'][1]
-cplt.map_allmonths(tst,lat,lon,ptype='nheur',
-                   cmin=-.15,cmax=.15,cmap='red2blue_w20')
+if printtofile:
+    fig.savefig(pstr+'_SITnh_diff_seacycle_BCs.pdf',bbox_inches='tight')
+    fig.savefig(pstr+'_SITnh_diff_seacycle_BCs.eps',bbox_inches='tight',format='eps',dpi=600)
+    fig2.savefig(pstr+'_SITnh_climo_seacycle_BCs.pdf',bbox_inches='tight')
+    fig2.savefig(pstr+'_SITnh_climo_seacycle_BCs.eps',bbox_inches='tight',format='eps',dpi=600)
+
+#tst=flddat['sicn'][1]
+#cplt.map_allmonths(tst,lat,lon,ptype='nheur',
+#                   cmin=-.15,cmax=.15,cmap='red2blue_w20')
 
 
 
 # @@@@@@@@@@@@@ also, plot regions for paper: supp fig (save code)
 cplt.plot_regions(('bksmori','eurasiamori'),colors=('k','blue'),ptype='nheur'); 
 if printtofile:
-    plt.savefig('suppfig_defineregions.pdf')
-    plt.savefig('suppfig_defineregions.eps',format='eps',dpi=600)
+    plt.savefig('suppfig_defineregions.pdf',bbox_inches='tight')
+    plt.savefig('suppfig_defineregions.eps',bbox_inches='tight',format='eps',dpi=600)
