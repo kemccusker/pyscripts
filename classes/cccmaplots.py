090ec2f8a6108bd2bd77e5832fcf58a5c7fbc82d
kemccusker <kellymccusker@gmail.com>
2016-04-13 17:39:29 -0700

    add print statement to add_regressline()

--- classes/cccmaplots.py
+++ classes/cccmaplots.py
@@ -1136,7 +1136,7 @@ def add_regressline(input1,input2,ax=None,axxlims=None,color='k',linewidth=1):
         axxlims = ax.get_xlim()
 
     mm,bb,rval,pval = cutl.regress(input1,input2)
-    print 'regress pval,rval: ' + str(pval),str(rval)
+    print 'regress mm,bb,pval,rval: ' + str(mm),str(bb),str(pval),str(rval)
 
     onex=np.linspace(axxlims[0],axxlims[1])    
     ax.plot(onex,mm*onex + bb, color=color,linewidth=linewidth)
